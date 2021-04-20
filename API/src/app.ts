3ee83e4904c1c64bd0c6cf474babf843319ef499
Nathan.M <nathanmoignard@gmail.com>
2021-04-21 00:51:56 +0200

    [ADD|UPDATE] Renaming Linter rules | Auto Compile and Restart API when developping

--- API/src/app.ts
+++ API/src/app.ts
@@ -1,11 +1,16 @@
 import mongoose from 'mongoose'
+import cors from 'cors'
 import express from 'express'
 import * as core from 'express-serve-static-core'
+import 'dotenv/config'
 
-const mongoUri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@drug-cluster.edgvn.mongodb.net/Drugs?retryWrites=true&w=majority`
-const mongoConfig = { useNewUrlParser: true, useUnifiedTopology: true }
+const mongoUri: string = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@drug-cluster.edgvn.mongodb.net/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`
+const mongoConfig: Object = { useNewUrlParser: true, useUnifiedTopology: true }
 
 const app: core.Express = express()
+
+app.use(cors())
+app.use(express.json())
 
 mongoose.connect(mongoUri, mongoConfig)
 .then(() => console.log(`Successfully connected to ${process.env.MONGO_DATABASE} Database`))
